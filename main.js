9535249e54eb1763d32f9ed87d099ba66fd91a1b
YeganaAbd <abd.yegane@gmail.com>
2021-11-22 16:39:33 +0000

    Task 3 and Task 4

--- main.js
+++ main.js
@@ -20,6 +20,34 @@ quoteClass.innerText = "It's raining man!";
 
 //////////// TASK 3
 
+// 👉 Select the h1 tag using querySelector and store in variable.
+let h1 = document.querySelector("h1");
+// 👉 Change its text-decoration property to the value of underline.
+h1.style.textDecoration = "underline";
+
+//Task 4
+// let lists = document.querySelectorAll("#tips-list");
+// for (let i = 1; i < lists.length; i++) {
+//   const newLi = document.createElement("li");
+
+//   lists[i].innerText = handleClick();
+//   lists.appendChild(newLi);
+// }
+let ul = document.querySelector("#tips-list");
+let lists = document.querySelectorAll("#tips-list>li");
+for (let i = 0; i < lists.length; i++) {
+  let newLi = document.createElement("li");
+  newLi.innerText = handleClick();
+  ul.appendChild(newLi);
+}
+// There is a list of extra tips to be added to the unordered list with the id of #tips-list
+// 👉 Select the tips list
+// 👉 Use a for loop to loop through the array of tips
+// 👉 for each tip...
+// create an li element
+// set the innerText of the li element to be the current tip
+// append the newly created element as a child of the ul
+
 let extraTips = [
   "Keep your pennies is a glass jar",
   "Save money buy not eating!",
